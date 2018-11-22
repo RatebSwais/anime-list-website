24b9284238e241845c98977defdba614e40e8484
Rateb <rateb@localhost.localdomain>
2018-11-22 12:16:08 +0200

    fixes

--- ani.js
+++ ani.js
@@ -1,21 +1,25 @@
-var query = `query($id: Int, $search: String) {
-  Character(id: $id, search: $search) {
-    name {
-      first
-      last
-      native
-      alternative
+var query = `{ 
+  Page(page: 1, perPage: 20){
+    
+    media(seasonYear: 2018, season: FALL){
+      title {
+        romaji
+        english
+        native
+        userPreferred
+      }
+      coverImage {
+        extraLarge
+      }
+      description
     }
-    image {
-      large
-    }
-    description
   }
+  
 }
 `;
 
 var variables = {
-  search: 'Makise'
+  search: 'Naruto'
 };
 
 var url = 'https://graphql.anilist.co', 
@@ -40,12 +44,24 @@ function handleResponse(response){
   return response.json().then(function(json) {
     if(response.ok){
 //return json;
-var object = json.data.Character.image.large; 
+var object = json.data.Page.media; 
 console.log(object);
+
+var out="";
+var out2="";
+for(var index in object){
+  if(object[index].coverImage !== null && object[index].title.romaji!== null){
+  out += "<div class='card' style='width: 20rem;'> <img style='width: 250px;' src='"+object[index].coverImage.extraLarge+"' class='card-img-top'> <div class='card-body'><h5>'"+object[index].title.romaji+"'</h5><p style='height: 130px; overflow-y: scroll;'>'"+object[index].description+"'</p></div></div>"  
+}
+}
+$(".grid-container").html(out);
+//$(".grid-container").html(out2);
+
 //var out = "<img width='200px' src='"+object+"'/>"
-document.getElementById("img1").src = object;
-document.getElementById("img2").src = object;
+//document.getElementById("img1").src = object;
+//document.getElementById("img2").src = object;
 //document.getElementById("display").innerHTML = out;
+
 }
 else{handleError();}
 });
