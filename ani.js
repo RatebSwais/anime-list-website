765c1bbae75149c0ead48136a475655ef70e5712
Rateb <rateb@localhost.localdomain>
2018-11-26 14:25:27 +0200

    fix

--- ani.js
+++ ani.js
@@ -1,63 +1,60 @@
-//Query sent to the api
-var query = `{ 
-    Page(page: 1, perPage: 20){
-      
-      media(season: FALL, seasonYear: 2018){
-        title {
-          romaji
-          english
-          native
-          userPreferred
-        }
-        coverImage {
-          extraLarge
-        }
-        description
-      }
-    }
     
-  }
-  `;
+    $(function(){
 
-  //API url and its options
-  var url = 'https://graphql.anilist.co', 
-  options = {
-    method: 'POST',
-    headers:{
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    },
-    body: JSON.stringify({
-      query: query
-    })
-  };
+    
+    const app = document.getElementById("root");
 
+    const container = document.createElement("div");
+    container.setAttribute('class', 'container'); 
 
-  //Making the request and handling the response
-  fetch(url, options).then(handleResponse)
-  .then(handleData)
-  .catch(handleError);
+    app.appendChild(container);
 
-  function handleResponse(response){
-    return response.json().then(function(json) {
-      if(response.ok){
-  var object = json.data.Page.media; 
-  console.log(object);
-  var out="";
-  for(var index in object){
-    if(object[index].coverImage !== null && object[index].title.romaji!== null){
-    out += "<div class='card' style='width: 20rem; height: auto;'> <img style='background-size: cover;' src='"+object[index].coverImage.extraLarge+"' class='card-img-top'> <div class='card-body'><h5>'"+object[index].title.romaji+"'</h5><p style='height: 130px; overflow-y: scroll; display: block;'>'"+object[index].description+"'</p></div></div>"  
-  }
-  }
-  $(".grid-container").html(out);
-  }
-  else{handleError();}
-  });
-  }
+    var query = `{ 
+        Page(page: 1, perPage: 20){
+        media(season: FALL, seasonYear: 2018){
+            title {
+            romaji
+            english
+            native
+            userPreferred
+            }
+            coverImage {
+            extraLarge
+            }
+            description
+        }
+        }    
+    }
+    `;
 
-  function handleData(data){
+    var request = new XMLHttpRequest(); 
+    request.open('POST', 'https://graphql.anilist.co/?query=' + query, true); 
+    request.onload = function(){
+    var data = JSON.parse(this.response);
+    var object = data.data.Page.media;
     console.log(data);
-  }
-  function handleError(error){
-    console.error(error);
-  }
+    if(request.status >= 200 && request.status < 400){
+        for (var index in object){
+           const card = document.createElement("div");
+           card.setAttribute('class', 'card');
+           const coverImage = document.createElement('img');
+           coverImage.src = object[index].coverImage.extraLarge;
+           const h1 = document.createElement("h1");
+           h1.textContent = object[index].title.romaji;
+           const description = document.createElement("p");
+           description.textContent = object[index].description;
+           container.appendChild(card);
+           card.appendChild(coverImage);
+           card.appendChild(h1);
+           card.appendChild(description);
+        };
+    }
+    else{
+        console.error(error);   
+    }
+    }
+
+    
+    request.send();
+
+});
